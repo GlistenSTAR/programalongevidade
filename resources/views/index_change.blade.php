6fef6b9e348fd8283e959633ae220cebd46312fe
GlistenSTAR <glistenstar00@gmail.com>
2021-04-13 11:39:30 -0700

    find_dragstroe select tag finished

--- resources/views/index_change.blade.php
+++ resources/views/index_change.blade.php
@@ -1,5 +1,3 @@
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="css/layout_main.css"/>
 @extends('layouts.layout')
 @section('content')
@@ -36,22 +34,22 @@
         </h1>
         <h4>O seu cadastro será feito de forma fácil e rápida</h4>
       </div>
-      <div class="container main">
-        <div class="row" align="center">
-          <div class="col-md-3" align="right">
-            <img class="block" id="u570_img" alt="" width="86" height="82" data-muse-src="images/home_icone_inicia_cadastro.png?crc=3891080647" src="images/home_icone_inicia_cadastro.png?crc=3891080647" data-nsfw-filter-status="sfw" style="visibility: visible;">
+        <div class="container main">
+          <div class="row" align="center">
+            <div class="col-md-3" align="right">
+              <img class="block" id="u570_img" alt="" width="86" height="82" data-muse-src="images/home_icone_inicia_cadastro.png?crc=3891080647" src="images/home_icone_inicia_cadastro.png?crc=3891080647" data-nsfw-filter-status="sfw" style="visibility: visible;">
+            </div>
+            <div class="col-md-4" style="margin-top:auto; margin-bottom:auto">
+              <input type="email" name="Email" class="form-control input-lg" placeholder="Vamos começar com o seu e-mail?" required>
+            </div>
+            <div class="col-md-3" style="margin-top:auto; margin-bottom:auto">
+              <button class="btn btn-primary form-control form-control-lg" id="go_register" type="submit">CADASTRAR AGORA</button>
+            </div>
           </div>
-          <div class="col-md-4" style="margin-top:auto; margin-bottom:auto">
-            <input type="email" class="form-control input-lg" placeholder="Vamos começar com o seu e-mail?">
-          </div>
-          <div class="col-md-3" style="margin-top:auto; margin-bottom:auto">
-            <button class="btn btn-primary form-control form-control-lg">CADASTRAR AGORA</button>
+          <div align="center" style="margin-bottom:28px">
+            <h4><a style="color:white">Já sou cadastrado. Como faço para acessar meus dados?</a></h4>
           </div>
         </div>
-        <div align="center" style="margin-bottom:28px">
-          <h4><a style="color:white">Já sou cadastrado. Como faço para acessar meus dados?</a></h4>
-        </div>
-      </div>
       <div class="dot_line"></div>
     </div>
   </div>
@@ -60,36 +58,68 @@
     <p>Localize a sua cidade pelos seletores abaixo</p>
     <div class="container mb-5">
       <div class="row">
-        <div class="col-md-2" align="right">
-          <img class="block" id="u603_img" alt="" width="86" height="82" data-muse-src="images/home_icone_localizar_farmacia.png?crc=4214829112" src="images/home_icone_localizar_farmacia.png?crc=4214829112" data-nsfw-filter-status="sfw" style="visibility: visible;">
-        </div>
-        <div class="col-md-7 form-group" align="left" style="margin-top:auto; margin-bottom:auto">
-          <select class="uf" id="ufID">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-          </select>
-          <select class="cidade" id="cidadeId">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-          </select>
-          <select class="bairro" id="bairroId">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-          </select>
-        </div>
-        <div class="col-md-3" align="left" style="margin-top:auto; margin-bottom:auto">
-          <button class="btn btn-primary form-control form-control-lg" style="width:200px!important;">CONSULTAR AGORA</button>
-        </div>
+          <div class="col-md-2" align="right">
+            <img class="block" id="u603_img" alt="" width="86" height="82" data-muse-src="images/home_icone_localizar_farmacia.png?crc=4214829112" src="images/home_icone_localizar_farmacia.png?crc=4214829112" data-nsfw-filter-status="sfw" style="visibility: visible;">
+          </div>
+          <div class="col-md-7 form-group" align="left" style="margin-top:auto; margin-bottom:auto">
+            <select class="uf" id="ufID">
+              <option value="" selected>UF</option>
+              @foreach ($laEstados as $estado)
+              <option value="{{$estado->ESTADO_ID}}">{{$estado->ESTADO_UF}}</option>
+              @endforeach
+            </select>
+            <select class="cidade" id="cidadeId">
+              <option value="" selected>Cidade</option>
+            </select>
+            <select class="bairro" id="bairroId">
+              <option value="" selected>Bairro</option>
+            </select>
+          </div>
+          <div class="col-md-3" align="left" style="margin-top:auto; margin-bottom:auto">
+            <button class="btn btn-primary form-control form-control-lg" id="find_dragstore" style="width:200px!important;">CONSULTAR AGORA</button>
+          </div>
       </div>
       <div class="mb-5">
         <p>Precisa de ajuda para consultar os pontos de venda?</p>
       </div>
     </div>
   </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+  @include("commons.estado_cidade_bairro")
+  <script>
+      $(document).ready(function () {
+        $("#go_register").on("click", function (event) {
+          // console.log(1);
+        var blockDiv = document.createElement('div');
+        $(blockDiv).css({'width' : '2200px', 'height' : '4200px', 'position' : 'absolute', 'z-index' : '1000000000000'});
+        $(blockDiv).html(" ");
+        $(event.currentTarget).parent().css({'position' : 'relative'});
+        $(event.currentTarget).parent().append(blockDiv);
+
+        event.preventDefault();
+
+        if (validateForm($('input'), null, null)) {
+            var form = generateForm('/cadastro_adesao', 'POST', '{{csrf_token()}}', $('input'), null, null);
+            $('body').append(form);
+            form.submit();
+        } else {
+            $(event.currentTarget).parent().css({'position' : 'static'});
+            $(blockDiv).remove();
+        }
+        });
+
+        $("#find_dragstore").on("click", function (event) {
+              // console.log("2");
+              event.preventDefault();
+
+              if (validateForm(null, $('select'), null)) {
+                  var form = generateForm('/busca_farmacias', 'POST', '{{csrf_token()}}', null, $('select'), null);
+                  $('body').append(form);
+                  form.submit();
+              }
+          });
+      });
+  </script>
 @endsection
