d4c7a3ef9c3a4e74a748eb8362af296dc800bc73
GlistenSTAR <glistenstar00@gmail.com>
2021-05-10 02:47:26 -0700

    contact_us_ok page add responsive

--- resources/views/!_contact_us_ok.blade.php
+++ resources/views/!_contact_us_ok.blade.php
@@ -2,16 +2,16 @@
 @section('content')
   <link rel="stylesheet" type="text/css" href="css/!contact_us.css"/>
   
-  <div class="container_fluid contact_us">
+  <div class="container_fluid contact_us contact_us_ok_letter">
     <div class="background"><div class="faca"></div>
       <div class="container">
-        <p class="faca_letter"><b>Mensagem recebida!</b></p>
+        <p class="faca_letter"><b>Mensagem recebida!</b></p> 
       </div>
     </div>
     <hr style="border-top:40px solid #2DBAD1; margin-top:0">
     <div class="container contact_us_ok mb-5"  align="center">
       <div class="row" style="width:80%" align="center">
-        <hr style="border-top:10px solid #2DBAD1; width:90%; margin-bottom:0">
+        <hr class="thin_hr" style="border-top:10px solid #2DBAD1; width:90%; margin-bottom:0">
         <h2 style="width:100%" align="center"><b>Agora basta aguardar. Nós faremos contato em breve!</b></h2>
         <p>Agradecemos pela sua mensagem. Nossa equipe leva em torno de 24h úteis para retornar os chamados. Acabamos de lhe enviar um e-mail com a confirmação de sua mensagem. Caso queira fazer contato por meio de outros meios, nosso endereço, nosso e-mail de contato e nosso 0800 estão disponíveis abaixo.</p>
       </div>
